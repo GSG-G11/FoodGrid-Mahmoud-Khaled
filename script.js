3c2f482ea2d6a40fa15df59e5656a24db015dc6c
khaled <yonkoluffy48@gmail.com>
2022-02-08 17:11:56 +0200

    create meals js relates #12

--- script.js
+++ script.js
@@ -1,79 +1,136 @@
-const countries=document.querySelector(".countries")
-const Section2=document.querySelector(".sec2")
-let countryUrl="https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+const countries = document.querySelector(".countries")
+const Section2 = document.querySelector(".sec2")
 
-function fetch(url,cb){
-let xhr=new XMLHttpRequest();
-xhr.onreadystatechange=()=>{
+const searchInput = document.querySelector(".search-content")
+const searchBtn = document.querySelector("#search-btn")
+const mealsCards = document.querySelector(".meals-container")
 
-if (xhr.readyState===4){
-switch (xhr.status){
-    case 200:
-let data=JSON.parse(xhr.responseText)
-console.log(data)
 
-cb(data)
+let countryUrl = "https://www.themealdb.com/api/json/v1/1/list.php?a=list"
+let mealsUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?a=American"
 
-break ;
-default:
+function fetch(url, cb) {
+    let xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
 
-    Section2.textContent=" NO Cuisine Available"
+        if (xhr.readyState === 4) {
+            switch (xhr.status) {
+                case 200:
+                    let data = JSON.parse(xhr.responseText)
+                    console.log(data)
 
-}}} 
-xhr.open("GET",url)
-xhr.send()
+                    cb(data)
+
+                    break;
+                default:
+
+                    Section2.textContent = " NO Cuisine Available"
+
+            }
+        }
+    }
+    xhr.open("GET", url)
+    xhr.send()
 
 
 
 }
 
-function cc(){
+function cc() {
 
     console.log("SSS")
 }
 
-function renderCountries(country){
-   
+function renderCountries(country) {
+
     country.meals.forEach(ele => {
-        
-      
 
-    //     <div class="cuisine">
-    //     <p class="country">British Cuisine</p>
-    //     <button class="go-meals">Show Meals</button>
-    //   </div>
-    const cuisineCard=document.createElement("div")
-    const countryNamePara=document.createElement("p")
-    const countryBtn=document.createElement("button")
-    const link=document.createElement("a")
+
+        const cuisineCard = document.createElement("div")
+        const countryNamePara = document.createElement("p")
+        const countryBtn = document.createElement("button")
+        const link = document.createElement("a")
 
 
 
-    cuisineCard.setAttribute("class","cuisine")
-    countryNamePara.setAttribute("class","country")
-    countryBtn.setAttribute("class","go-meals")
+        cuisineCard.setAttribute("class", "cuisine")
+        countryNamePara.setAttribute("class", "country")
+        countryBtn.setAttribute("class", "go-meals")
 
 
-    countryBtn.textContent="Show Meals"
+        countryBtn.textContent = "Show Meals"
 
 
-    countries.appendChild(cuisineCard)
-    cuisineCard.appendChild(countryNamePara)
-    cuisineCard.appendChild(link)
-    link.appendChild(countryBtn)
-    
+        countries.appendChild(cuisineCard)
+        cuisineCard.appendChild(countryNamePara)
+        cuisineCard.appendChild(link)
+        link.appendChild(countryBtn)
 
-    countryNamePara.textContent=ele.strArea
-    link.href="./meals.html"
+
+        countryNamePara.textContent = ele.strArea
+        link.href = "./meals.html"
 
     });
-       
-   
-
-
-
-
 }
 
 
-fetch(countryUrl,renderCountries)
+
+function search(obj) {
+
+    searchBtn.addEventListener("click", () => {
+
+        mealsCards.textContent= ""
+
+        obj.meals.forEach(ele => {
+
+        
+            if(ele.strMeal.toLowerCase().includes(searchInput.value.toLowerCase())) {
+                const mealDiv = document.createElement("div")
+                const mealImg = document.createElement("img")
+                const mealTitle = document.createElement("p")
+        
+                mealDiv.setAttribute("class", "meal")
+                mealImg.setAttribute("class", "meal-photo")
+                mealTitle.setAttribute("class", "meal-name")
+        
+                mealsCards.appendChild(mealDiv)
+                mealDiv.appendChild(mealImg)
+                mealDiv.appendChild(mealTitle)
+        
+        
+                mealTitle.textContent = ele.strMeal
+                mealImg.src = ele.strMealThumb
+            }
+        })
+
+    })
+}
+
+function mealcard(obj) {
+    obj.meals.forEach(ele => {
+
+
+        const mealDiv = document.createElement("div")
+        const mealImg = document.createElement("img")
+        const mealTitle = document.createElement("p")
+
+        mealDiv.setAttribute("class", "meal")
+        mealImg.setAttribute("class", "meal-photo")
+        mealTitle.setAttribute("class", "meal-name")
+
+        mealsCards.appendChild(mealDiv)
+        mealDiv.appendChild(mealImg)
+        mealDiv.appendChild(mealTitle)
+
+
+        mealTitle.textContent = ele.strMeal
+        mealImg.src = ele.strMealThumb
+        
+
+    })
+}
+
+
+fetch(countryUrl, renderCountries)
+fetch(mealsUrl, mealcard)
+fetch(mealsUrl, search)
